98833a114cde859cb4f967d688cd8ed81a2f3b9e
János Veres <janos@dot.industries>
2022-10-23 08:48:08 +0200

    feat: expose api merged oas3 spec /api/spec

--- apps/api/pages/docs/swagger.tsx
+++ apps/api/pages/docs/swagger.tsx
@@ -1,8 +1,7 @@
-import { seatsApi } from "@dotinc/bouncer-core";
-import { toOpenApi } from "@zodios/openapi";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { createSwaggerSpec } from "next-swagger-doc";
 import dynamic from "next/dynamic";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { apiDefinition } from "@dotinc/bouncer-core";
+import { createSwaggerSpec } from "next-swagger-doc";
 import "swagger-ui-react/swagger-ui.css";
 
 const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
@@ -12,21 +11,8 @@ function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const definition = toOpenApi(seatsApi, {
-    info: {
-      title: "Seats API",
-      version: "1.0.0",
-      description: "SaaS seat management API",
-    },
-    servers: [
-      {
-        url: "/api/v1", // base path of user api
-      },
-    ],
-  });
-
   const spec: Record<string, any> = createSwaggerSpec({
-    definition,
+    definition: apiDefinition,
   });
 
   return {
