02b5ad9706257ede6fbb9bfcfe8d759fccd6d425
János Veres <janos@dot.industries>
2022-10-24 22:33:14 +0200

    fix: remove nextjs config workaround for tanstack

--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -21,15 +21,5 @@ export default // withTM(
 defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ["@tanstack/react-query", ...config.externals];
-    }
-
-    const reactQuery = path.resolve(require.resolve("@tanstack/react-query"));
-
-    config.resolve.alias["@tanstack/react-query"] = reactQuery;
-    return config;
-  },
 });
 // );
