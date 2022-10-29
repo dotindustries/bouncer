d7fa44c75abaad0c769b6ec7894f6b3d05df2eac
Eelco Wiersma <contact@pagebakers.nl>
2022-10-29 20:33:36 +0200

    feat: fix formatting

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -1,18 +1,18 @@
-import { Zodios, ZodiosOptions } from '@zodios/core'
+import { Zodios, ZodiosOptions } from "@zodios/core";
 
-import { seatsApi, Reservation, User, user } from '@dotinc/bouncer-core'
+import { seatsApi, Reservation, User, user } from "@dotinc/bouncer-core";
 
-import type { InferParams } from './types'
+import type { InferParams } from "./types";
 
 export interface BouncerClientOptions extends ZodiosOptions {
-  apiKey: string
-  attr?: User
+  apiKey: string;
+  attr?: User;
 }
 
 export const createClient = (options: BouncerClientOptions) => {
-  const { apiKey, axiosConfig, axiosInstance, validate, attr } = options
+  const { apiKey, axiosConfig, axiosInstance, validate, attr } = options;
 
-  let _attr = attr && user.parse(attr)
+  let _attr = attr && user.parse(attr);
 
   const zodiosOptions: ZodiosOptions = {
     axiosInstance,
@@ -21,27 +21,27 @@ export const createClient = (options: BouncerClientOptions) => {
       ...axiosConfig,
       headers: {
         ...axiosConfig?.headers,
-        'x-api-key': apiKey,
+        "x-api-key": apiKey,
       },
     },
-  }
+  };
 
-  const api = new Zodios('/api/v1', seatsApi, zodiosOptions)
+  const api = new Zodios("/api/v1", seatsApi, zodiosOptions);
 
-  const { redeemSeat, requestSeat, reserveSeat, releaseSeat } = api
+  const { redeemSeat, requestSeat, reserveSeat, releaseSeat } = api;
 
   const seats = {
     redeem: async (params: InferParams<typeof redeemSeat>) => {
-      const attr = user.parse(_attr)
+      const attr = user.parse(_attr);
       return redeemSeat(attr, {
         params,
-      })
+      });
     },
     request: async (params: InferParams<typeof requestSeat>) => {
-      const attr = user.parse(_attr)
+      const attr = user.parse(_attr);
       return requestSeat(attr, {
         params,
-      })
+      });
     },
     reserve: (
       params: InferParams<typeof reserveSeat>,
@@ -49,19 +49,19 @@ export const createClient = (options: BouncerClientOptions) => {
     ) => {
       return reserveSeat(reservation, {
         params,
-      })
+      });
     },
     release: (params: InferParams<typeof releaseSeat>) => {
       return releaseSeat(undefined, {
         params,
-      })
+      });
     },
-  }
+  };
 
   return {
     identify: (attr: Attr) => {
-      _attr = user.parse(attr)
+      _attr = user.parse(attr);
     },
     seats,
-  }
-}
+  };
+};
