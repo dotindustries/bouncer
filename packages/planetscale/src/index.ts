e9fd3c958d713a91f7aaf2ace876c1ed06d163b0
János Veres <janos@dot.industries>
2022-11-01 13:57:08 +0100

    fix: reading number instead of boolean from planetscale

--- packages/planetscale/src/index.ts
+++ packages/planetscale/src/index.ts
@@ -5,6 +5,8 @@ import {
 } from "kysely-planetscale";
 import type { ZodiosPlugin } from "@zodios/core";
 import { createRepository as createBaseRepository } from "@dotinc/bouncer-sql";
+import { parseJSON } from "date-fns";
+import { cast } from "@planetscale/database";
 
 export { migrateToLatest } from "./migration";
 
@@ -12,7 +14,16 @@ export const createRepository = (
   config: PlanetScaleDialectConfig
 ): Repository => {
   return createBaseRepository({
-    dialect: new PlanetScaleDialect(config),
+    dialect: new PlanetScaleDialect({
+      cast: (field, value) => {
+        if (field.type === "DATETIME" && value) return parseJSON(value);
+        if (field.type === "TIMESTAMP" && value) return parseJSON(value);
+        if (field.type === "INT8" && value !== null && value !== undefined)
+          return parseInt(value) === 1 ? true : false;
+        return cast(field, value);
+      },
+      ...config, // allow for override
+    }),
     log: ["query", "error"],
   });
 };
