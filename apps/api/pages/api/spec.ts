20c5790536a6a1ee2685dfa0811faafe60a34a84
János Veres <janos@dot.industries>
2023-01-25 20:02:42 +0100

    refactor: openApi spec

--- apps/api/pages/api/spec.ts
+++ apps/api/pages/api/spec.ts
@@ -1,6 +1,9 @@
 import { api } from "@dotinc/bouncer-core";
 import { NextApiRequest, NextApiResponse } from "next";
+import { env } from "~/env/server.mjs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(api);
+  res
+    .status(200)
+    .json(api(env.SPEAKEASY_WORKSPACE_ID ?? "your-speakeasy-workspace-id"));
 }
